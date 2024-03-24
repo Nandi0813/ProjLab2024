ae03956988163ccae7403a8a520f776612284aa3
Kurtos András <andraskurtos@gmail.com>
2024-03-24 21:11:27 +0100

    made run.bat

---
+++ core/run.bat
@@ -0,0 +1,8 @@
+IF EXIST bin rmdir /s/q bin
+mkdir bin
+
+SET "sources=src/main/java/com/bucikft/Items/.java src/main/java/com/bucikft/Door/.java src/main/java/com/bucikft/Person/.java src/main/java/com/bucikft/Tests/.java src/main/java/com/bucikft/*.java"
+
+javac -encoding UTF-8 -d .\bin %sources% 2>nul
+
+java -cp bin com.bucikft.SkeletonMenu
