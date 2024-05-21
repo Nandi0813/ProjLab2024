3e7bb87a18639f2cd357490b1c71ea0797963166
Kurtos András <andraskurtos@gmail.com>
2024-05-21 23:03:48 +0200

    run.bat made

--- core/run.bat
+++ core/run.bat
@@ -5,11 +5,11 @@ IF EXIST bin (
 )
 mkdir bin
 
-SET "sources=src\main\java\com\bucikft\commands\*.java src\main\java\com\bucikft\Door\*.java src\main\java\com\bucikft\Items\*.java src\main\java\com\bucikft\Person\*.java src\main\java\com\bucikft\*.java src\main\java\com\bucikft\Items\Interface\*.java"
+SET "sources=src\main\java\com\bucikft\Controllers\*.java src\main\java\com\bucikft\Door\*.java src\main\java\com\bucikft\Items\*.java src\main\java\com\bucikft\Person\*.java src\main\java\com\bucikft\*.java src\main\java\com\bucikft\Items\Interface\*.java src\main\java\com\bucikft\Views\*.java src\main\java\com\bucikft\Utils\*.java"
 
 javac -encoding UTF-8 -d bin %sources%
 
-java -cp bin com.bucikft.Menu
+java -cp bin com.bucikft.Views.MenuView
 
 echo Press Ctrl+C to exit or any key to restart...
 pause > nul
---
+++ run.bat
@@ -0,0 +1,16 @@
+@echo off
+:loop
+IF EXIST bin (
+    rmdir /s /q bin
+)
+mkdir bin
+
+SET "sources=src\main\java\com\bucikft\commands\*.java src\main\java\com\bucikft\Door\*.java src\main\java\com\bucikft\Items\*.java src\main\java\com\bucikft\Person\*.java src\main\java\com\bucikft\*.java src\main\java\com\bucikft\Items\Interface\*.java"
+
+javac -encoding UTF-8 -d bin %sources%
+
+java -cp bin com.bucikft.Menu
+
+echo Press Ctrl+C to exit or any key to restart...
+pause > nul
+goto loop
